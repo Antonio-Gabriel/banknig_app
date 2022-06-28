a6f37c649823ba17ff212b1827e6c9bda31f9ae4
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-06-28 22:06:04 +0100

    sort movements

--- script.js
+++ script.js
@@ -61,8 +61,10 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount')
 const inputCloseUsername = document.querySelector('.form__input--user')
 const inputClosePin = document.querySelector('.form__input--pin')
 
-const displayMovements = (movements) => {
-  movements.forEach((mov, i) => {
+const displayMovements = (movements, sort = false) => {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements
+
+  movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
 
     const html = `
@@ -239,6 +241,16 @@ btnClose.addEventListener('click', (e) => {
   } else {
     alert('Please, check your credentials for close an account')
   }
+})
+
+// Sort movements
+let sorted = false
+btnSort.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  displayMovements(currentAccount.movements, !sorted)
+
+  sorted = !sorted
 })
 
 // LECTURES
